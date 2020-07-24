6d223d8d64b585313cf2458a63e28d9518abab55
nhcarrigan <nhcarrigan@gmail.com>
2020-07-24 14:11:01 -0700

    Restructure files - I has idea

---
+++ build/precache-manifest.2e1fc336a87d26b95fc7bad9efddaac2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e831103b0e518f327bf0fe24fbc0f41",
+    "url": "/nhcarrigan/mern-todo-appindex.html"
+  },
+  {
+    "revision": "0d57cc9dfb15ab37dd02",
+    "url": "/nhcarrigan/mern-todo-appstatic/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f95f2a234308b53a98e6",
+    "url": "/nhcarrigan/mern-todo-appstatic/js/2.996a91dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nhcarrigan/mern-todo-appstatic/js/2.996a91dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d57cc9dfb15ab37dd02",
+    "url": "/nhcarrigan/mern-todo-appstatic/js/main.1fd7e73b.chunk.js"
+  },
+  {
+    "revision": "15eb6a62b1c2c6945f51",
+    "url": "/nhcarrigan/mern-todo-appstatic/js/runtime-main.af80a43f.js"
+  }
+]);
